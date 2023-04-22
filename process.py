4715d3c40970c72d798949bf4d8d75c6ba922f46
Immortalise <kaijiezhu11@gmail.com>
2023-04-22 05:42:56 -0700

    version 0.1, only support pdf format

--- process.py
+++ process.py
@@ -1,13 +1,10 @@
-import PyPDF2  
-from pdfminer.high_level import extract_metadata  
-from pdfminer.pdfparser import PDFParser  
-from pdfminer.pdfdocument import PDFDocument  
+from pypdf import PdfReader
 from utils import encode_text
 
 
 def process_file(file_path, tokenizer, model):
     if file_path.endswith(".pdf"):
-        process_pdf(file_path, tokenizer, model)
+        return process_pdf(file_path, tokenizer, model)
     else:
         print("File type not supported.")
 
@@ -15,34 +12,49 @@ def process_file(file_path, tokenizer, model):
 def process_pdf(pdf_file_path, tokenizer, model):
     line_list = []
   
-    # Extract metadata  
-    with open(pdf_file_path, 'rb') as file:  
-        parser = PDFParser(file)  
-        document = PDFDocument(parser)  
-        metadata = extract_metadata(document)  
-    
-    pdf_title = metadata['title'] if 'title' in metadata else None  
-    pdf_author = metadata['author'] if 'author' in metadata else None   
-    
-    # Extract text content and track the corresponding page for each line  
-    with open(pdf_file_path, 'rb') as file:  
-        reader = PyPDF2.PdfFileReader(file)  
-        num_pages = reader.numPages  
+    reader = PdfReader(pdf_file_path)
+    meta = reader.metadata
+
+    # All of the following could be None!
+    pdf_title = meta.title
+    pdf_author = meta.author
+    pdf_subject = meta.subject
+            
+    for page_num, page in enumerate(reader.pages):
+
+        text = page.extract_text()
+
+        # Split text into lines and store them with their corresponding page number  
+        raw_lines = text.splitlines()
         
-        for page_num in range(num_pages):  
-            page = reader.getPage(page_num)  
-            text = page.extract_text()  
+        raw_lines = [raw_line.lower() for raw_line in raw_lines]
 
-            # Split text into lines and store them with their corresponding page number  
-            lines = text.splitlines()  
-            for line in lines:
-                file_dict = {}
-                file_dict['title'] = pdf_title
-                file_dict['author'] = pdf_author
-                file_dict['page'] = page_num + 1 
-                file_dict['content'] = line
-                file_dict['embedding'] = encode_text(tokenizer, model, line)
+        contains_references = any(raw_line == 'references' for raw_line in raw_lines)
 
-                line_list.append(file_dict)
-    
-    return file_dict
+        if contains_references:
+            raw_lines = raw_lines[:raw_lines.index('references')]
+        
+        concatenated_string = ' '.join(raw_lines)
+        concatenated_string.replace("- ", "")
+
+
+        lines = concatenated_string.split('\n')
+
+        for line in lines:
+            print(line)
+
+            file_dict = {}
+            file_dict['title'] = pdf_title
+            file_dict['author'] = pdf_author
+            file_dict['page'] = page_num + 1
+            file_dict['content'] = line
+            file_dict['embedding'] = encode_text(tokenizer, model, line)
+            file_dict['file_path'] = pdf_file_path
+            file_dict['subject'] = pdf_subject
+
+            line_list.append(file_dict)
+
+        if contains_references:
+            break
+
+    return line_list
