5c7fbc40b8afce4fc02d235a59972cb6faa9ae60
Immortalise <u201714689@hust.edu.cn>
2023-05-05 05:25:46 -0700

    sync

--- filegpt.py
+++ filegpt.py
@@ -1,12 +1,9 @@
-from watchdog.events import FileSystemEventHandler
-from watchdog.observers import Observer
-import pickle
 import os
 from sentence_transformers import SentenceTransformer
 
-from config import MONITORED_DIRS_PATH, DATA_DIR, SUPPORTED_FILE_TYPE
+from config import DATA_DIR, SUPPORTED_FILE_TYPE
 from database import DataBase
-from utils import encode_text, filter_subdirectories, list_files_with_type
+from utils import encode_text, list_files_with_type
 from process import process_file
 from index import Index
 
@@ -109,55 +106,10 @@ class FileGPT(object):
         self.index.close()
 
 
-class FileChangeHandler(FileSystemEventHandler):
-    def __init__(self, filegpt: FileGPT):
-        
-        self.filegpt = filegpt
-
-        if not os.path.exists(MONITORED_DIRS_PATH):
-            self.monitored_dirs = set()
-        else:
-            with open(MONITORED_DIRS_PATH, 'rb') as file:  
-                self.monitored_dirs = pickle.load(file)
-                    
-
-    def on_created(self, event):  
-        print(f"{'Directory' if event.is_directory else 'File'} added: {event.src_path}")  
-        self.filegpt.insert(event.src_path)
-  
-    def on_deleted(self, event):
-        print(f"{'Directory' if event.is_directory else 'File'} deleted: {event.src_path}")  
-        self.filegpt.delete(event.src_path)
-        self.monitored_dirs.remove(event.src_path)
-  
-    def on_modified(self, event):  
-        print(f"{'Directory' if event.is_directory else 'File'} modified: {event.src_path}")
-        self.filegpt.delete(event.src_path)
-        self.filegpt.insert(event.src_path)
-  
-    def on_moved(self, event):
-        print(f"{'Directory' if event.is_directory else 'File'} moved from {event.src_path} to {event.dest_path}")
-    
-    def close(self):
-        with open(MONITORED_DIRS_PATH, 'wb') as file:
-            self.monitored_dirs = filter_subdirectories(self.monitored_dirs)
-            pickle.dump(self.monitored_dirs, file)
-
-
 if __name__ == "__main__":
     filegpt = FileGPT("sentence-transformers/all-mpnet-base-v2")
-
-    event_handler = FileChangeHandler(filegpt)
-    observer = Observer()
-    
-    for directory in event_handler.monitored_dirs:
-        observer.schedule(event_handler, directory, recursive=True)
-    
-    observer.start()
-    event_handler.close()
     filegpt.run()
-    observer.stop()
-    observer.join()
+
 
 
 
